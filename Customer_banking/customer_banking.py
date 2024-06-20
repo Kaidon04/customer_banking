9bd1fcc1de47dab0550a8c3d652fcda338058325
Kaidon04 <kaidonkennedy@Kaidons-Laptop.local>
2024-06-20 11:01:59 -0600

    commit D

--- Customer_banking/customer_banking.py
+++ Customer_banking/customer_banking.py
@@ -14,7 +14,7 @@ def main():
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
-    # ADD YOUR CODE HERE
+    
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
@@ -27,3 +27,4 @@ def main():
 
 if __name__ == "__main__":
     # Call the main function.
+ 
